40a4217c3f260fcaf5a7e32c877032e07b2ab166
Leo Ss <leo.vm.cba@gmail.com>
2026-03-10 20:00:19 -0300

    ✅ /02-pokemon-static: props and components to islands

--- 02-pokemon-static/src/components/Counter.tsx
+++ 02-pokemon-static/src/components/Counter.tsx
@@ -1,22 +1,30 @@
-import { createSignal } from "solid-js"
+import { createSignal, type Component } from "solid-js";
 
-export const Counter = () => {
-    const [ counter, setCounter ] = createSignal(10);
+interface Props {
+    initialValue: number;
+}
+
+export const Counter: Component<Props> = (props) => {
+    const [ counter, setCounter ] = createSignal(props.initialValue);
 
     return (
 
         <>
-            <h1>Counter</h1>
-            <h3>Value: { counter() } </h3>
+            <div class="flex flex-col gap-3 bg-indigo-950 py-6 px-8 rounded-md">
+                <h1>Counter</h1>
+                <h3>Value: { counter() } </h3>
 
-            <button
-                class="py-2 px-4 bg-blue-500 text-white rounded cursor-pointer"
-                onClick={() => setCounter( prev => ++prev)}> + 
-                </button>
-            <button
-                class="py-2 px-4 bg-red-500 text-white rounded cursor-pointer"
-                onClick={() => setCounter( prev => --prev)} > - 
-                </button>
+                <div class="flex gap-x-5">
+                    <button
+                        class="py-2 px-4 bg-blue-500 text-white font-bold rounded cursor-pointer"
+                        onClick={() => setCounter( prev => ++prev)}> + 
+                        </button>
+                    <button
+                        class="py-2 px-4 bg-red-500 text-white font-bold rounded cursor-pointer"
+                        onClick={() => setCounter( prev => --prev)} > -
+                    </button>
+                </div>
+            </div>
         </>
     )
 }
