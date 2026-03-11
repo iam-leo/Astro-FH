25e614bb7309d26e603b6006cebbb3f02898b6da
Leo Ss <leo.vm.cba@gmail.com>
2026-03-10 21:23:53 -0300

    ✅ /02-pokemon-static: add view-transitions from island to pokemon page

--- 02-pokemon-static/src/components/pokemons/FavoritePokemonCard.tsx
+++ 02-pokemon-static/src/components/pokemons/FavoritePokemonCard.tsx
@@ -26,7 +26,11 @@ export const FavoritePokemonCard: Component<Props> = ({ pokemon }) => {
             <div class="bg-slate-800 p-3 rounded flex flex-col items-center justify-center">
                 <a href={`/pokemons/${pokemon.name}`} class="flex flex-col items-center justify-center">
                     <h3 class="capitalize text-white">{pokemon.name}</h3>
-                    <img src={imageUrl} alt={pokemon.name} class="size-24"/>
+                    <img 
+                        src={imageUrl}
+                        class="size-24"
+                        alt={pokemon.name}
+                        style={`view-transition-name: ${pokemon.name}-image`}/>
                 </a>
 
                 <button
