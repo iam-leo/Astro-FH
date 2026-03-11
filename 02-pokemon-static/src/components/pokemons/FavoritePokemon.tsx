024020455d0f595e940963b936491a03f42d9241
Leo Ss <leo.vm.cba@gmail.com>
2026-03-10 21:16:45 -0300

    ✅ /02-pokemon-static: show and remove favorite pokemons

--- 02-pokemon-static/src/components/pokemons/FavoritePokemon.tsx
+++ 02-pokemon-static/src/components/pokemons/FavoritePokemon.tsx
@@ -1,5 +1,6 @@
 import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
 import { createSignal, For } from "solid-js";
+import { FavoritePokemonCard } from "./FavoritePokemonCard";
 
 const getLocalStoragePokemons = (): FavoritePokemon[] => {
     const favoritesPokemons = JSON.parse(localStorage.getItem('favorites') ?? '[]');
@@ -14,9 +15,7 @@ export const FavoritePokemons = () => {
         <div class="m-5 grid grid-cols-4 gap-3">
             <For each={pokemons()}>
                 {(pokemon) => (
-                    <div class="bg-slate-800 p-3 rounded">
-                        <h3 class="capitalize text-white">{pokemon.name}</h3>
-                    </div>
+                    <FavoritePokemonCard pokemon={pokemon} />
                 )}
             </For>
         </div>
---
+++ 02-pokemon-static/src/components/pokemons/FavoritePokemonCard.tsx
@@ -0,0 +1,40 @@
+import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
+import { createSignal, Show, type Component } from "solid-js";
+
+
+interface Props {
+    pokemon: FavoritePokemon;
+}
+
+export const FavoritePokemonCard: Component<Props> = ({ pokemon }) => {
+    const [ isVisible, setIsVisible ] = createSignal(true);
+
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+
+    const deleteFavorite = () => {
+        const favoritesPokemons = JSON.parse(localStorage.getItem('favorites') ?? '[]') as FavoritePokemon[];
+
+        const updatedFavorites = favoritesPokemons.filter((fav: FavoritePokemon) => fav.id !== pokemon.id);
+
+        localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+
+        setIsVisible(false);
+    }
+
+    return (
+        <Show when={ isVisible() }>
+            <div class="bg-slate-800 p-3 rounded flex flex-col items-center justify-center">
+                <a href={`/pokemons/${pokemon.name}`} class="flex flex-col items-center justify-center">
+                    <h3 class="capitalize text-white">{pokemon.name}</h3>
+                    <img src={imageUrl} alt={pokemon.name} class="size-24"/>
+                </a>
+
+                <button
+                    onClick={deleteFavorite}
+                    class="bg-red-600 hover:bg-red-700 text-white px-3 py-2 rounded mt-2 cursor-pointer"
+                >Eliminar
+                </button>
+            </div>
+        </Show>
+    )
+}
